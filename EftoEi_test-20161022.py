b595f0b2aef6377beb60f280672b236512a04f39
Jørgen Eriksson Midtbø <j.e.midtbo@fys.uio.no>
2016-10-22 15:00:20 -0400

    Think I have the EitoEf and EftoEi right now. At least theyre consistent when applied alternatingly and iteratively. But there is still some error in the rhosigchi iteration -- the first nontrivial iteration actually looks pretty good, but then something happens and it starts removing more and more of the low-Eg-high-Ex counts, which it shouldnt do. Need to go through the rhosichi iterator line by line.

--- EftoEi_test-20161022.py
+++ EftoEi_test-20161022.py
@@ -9,29 +9,38 @@ a2d = a2d*b2d
 
 plt.figure()
 
-plt.subplot(2,2,1)
+plt.subplot(3,2,1)
 plt.title('original')
 plt.pcolormesh(axis,axis,a2d,vmin=a2d.min(), vmax=a2d.max())
 print "original:"
 print a2d
 
-plt.subplot(2,2,2)
+plt.subplot(3,2,2)
 plt.title('EitoEf')
 plt.pcolormesh(axis,axis,EitoEf(a2d,axis),vmin=a2d.min(), vmax=a2d.max())
 print "EitoEf:"
 print EitoEf(a2d,axis)
 
-plt.subplot(2,2,3)
+plt.subplot(3,2,3)
 plt.title('EftoEi(EitoEf)')
 plt.pcolormesh(axis,axis,EftoEi(EitoEf(a2d,axis),axis),vmin=a2d.min(), vmax=a2d.max())
 print "EftoEi(EitoEf):"
 print EftoEi(EitoEf(a2d,axis),axis)
 
-plt.subplot(2,2,4)
+plt.subplot(3,2,4)
 plt.title('EitoEi(EitoEf(EftoEi))')
 plt.pcolormesh(axis,axis,EitoEf(EftoEi(EitoEf(a2d,axis),axis),axis),vmin=a2d.min(), vmax=a2d.max())
 print "EitoEi(EitoEf(EftoEi)):"
-print EftoEi(EitoEf(a2d,axis),axis)
+print EitoEf(EftoEi(EitoEf(a2d,axis),axis),axis)
+
+plt.subplot(3,2,5)
+plt.title('..and again..')
+plt.pcolormesh(axis,axis,EftoEi(EitoEf(EftoEi(EitoEf(a2d,axis),axis),axis),axis),vmin=a2d.min(), vmax=a2d.max())
+
+plt.subplot(3,2,6)
+plt.title('..and again..')
+plt.pcolormesh(axis,axis,EitoEf(EftoEi(EitoEf(EftoEi(EitoEf(a2d,axis),axis),axis),axis),axis),vmin=a2d.min(), vmax=a2d.max())
+
 
 plt.show()
 
